fb8268a24db3c810fee1b9cc6002720e422103da
montella1507 <marian.bencat@live.com>
2022-06-06 14:19:15 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -1,78 +1,87 @@
 // styles
 import './style.css';
-// rete base 
-import '@babel/polyfill'
-import { Node } from "rete";
-import Rete, {Socket, Component, Input } from "rete";
+// rete base
+import '@babel/polyfill';
+import { Node } from 'rete';
+import Rete, { Socket, Component, Input } from 'rete';
 import ConnectionPlugin from 'rete-connection-plugin';
 import VueRenderPlugin from 'rete-vue-render-plugin';
-import { NodeData,WorkerInputs,WorkerOutputs } from 'rete/types/core/data';
+import { NodeData, WorkerInputs, WorkerOutputs } from 'rete/types/core/data';
 import ContextMenuPlugin from 'rete-context-menu-plugin';
 
-// utility 
+// utility
 import { download } from './download';
-// Data 
+// Data
 import jsonData from './data.json';
 
 // Strongly-typing
 import { Data, Pass } from './models';
 
-
 // Factory method returning builders
-const createComponent = (pass: Pass, sockets: Socket[])  => {
+const createComponent = (pass: Pass, sockets: Socket[]) => {
   return class extends Component {
-  builder(node: Node): Promise<void> {
+    builder(node: Node): Promise<void> {
       // pridame inputy
-     pass.inputs.forEach(input => {
-      const foundSocket = sockets.find(s => s.name == input.type);
-      const newInput = new Rete.Input(pass.name + "/" + input.name, input.name, foundSocket);
-      node.addInput(newInput);
-     });
+      pass.inputs.forEach((input) => {
+        const foundSocket = sockets.find((s) => s.name == input.type);
+        const newInput = new Rete.Input(
+          pass.name + '::' + input.name,
+          input.name,
+          foundSocket
+        );
+        node.addInput(newInput);
+      });
 
-        // pridame outputy
-        pass.outputs.forEach(input => {
-          const foundSocket = sockets.find(s => s.name == input.type);
-          const newOutput = new Rete.Output(pass.name + "/" + input.name, input.name, foundSocket);
-          node.addOutput(newOutput);
-         });
-     
-    return Promise.resolve();
-  }
-  worker(node: NodeData,inputs: WorkerInputs,outputs: WorkerOutputs,...args: unknown[]): void {}
+      // pridame outputy
+      pass.outputs.forEach((input) => {
+        const foundSocket = sockets.find((s) => s.name == input.type);
+        const newOutput = new Rete.Output(
+          pass.name + '::' + input.name,
+          input.name,
+          foundSocket
+        );
+        node.addOutput(newOutput);
+      });
 
-  constructor() {
-     super(pass.name);
-  }
-  }
-}
+      return Promise.resolve();
+    }
+    worker(
+      node: NodeData,
+      inputs: WorkerInputs,
+      outputs: WorkerOutputs,
+      ...args: unknown[]
+    ): void {}
 
+    constructor() {
+      super(pass.name);
+    }
+  };
+};
 
 // SETUP
 const data: Data = jsonData;
-const container = document.getElementById("rete");
+const container = document.getElementById('rete');
 const editor = new Rete.NodeEditor('demo@0.1.0', container);
 editor.use(ConnectionPlugin);
 editor.use(VueRenderPlugin);
 editor.use(ContextMenuPlugin, {
-  searchBar: false, 
-  searchKeep: title => true,
+  searchBar: false,
+  searchKeep: (title) => true,
   delay: 100,
   rename(component) {
-      return component.name;
+    return component.name;
   },
   nodeItems: {
-      'Delete': true, 
-      'Clone': true 
-  }
+    Delete: true,
+    Clone: true,
+  },
 });
 
-
 // Passes definitions
-const sockets = data.valueTypes.map(vType => new Rete.Socket(vType.name));
-const factories = data.passes.map(def => createComponent(def, sockets));
-const builders = factories.map(f => new f());
-builders.forEach(b => editor.register(b));
-
+const sockets = data.valueTypes.map((vType) => new Rete.Socket(vType.name));
+const factories = data.passes.map((def) => createComponent(def, sockets));
+const builders = factories.map((f) => new f());
+builders.forEach((b) => editor.register(b));
 
 // instances - uncomment to test
 // data.instances.forEach(async instance=> {
@@ -82,18 +91,21 @@ builders.forEach(b => editor.register(b));
 //   editor.addNode(node);
 // });
 
-
-editor.on('nodecreated noderemoved connectioncreated connectionremoved', async () => {
-  console.log(editor.toJSON());
-});
-
-
+editor.on(
+  'nodecreated noderemoved connectioncreated connectionremoved',
+  async () => {
+    console.log(editor.toJSON());
+  }
+);
 
 window.saveToFile = () => {
-
   let fileContent = {
     ...data,
-    editor: editor.toJSON()
+    editor: editor.toJSON(),
   };
- download(JSON.stringify(fileContent, null, 2), "data.json", "application/json");
+  download(
+    JSON.stringify(fileContent, null, 2),
+    'data.json',
+    'application/json'
+  );
 };
